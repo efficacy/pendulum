a370d63429a235b3c96ce9d20de2dffe4f6d4e49
Frank Carver <frank@efsol.com>
2015-02-20 10:48:00 +0000

    use callbacks on external calls

--- src/pendulum.js
+++ src/pendulum.js
@@ -72,36 +72,44 @@ module.exports = function instance(workspace, home, project) {
         });
       });
     },
+
     link: function link(callback) {
       var self = this;
       async.each(self.modules, function(module, done) {
         process.chdir(path.dirname(self.location[module]));
-        var child = exec('npm link').stderr.pipe(process.stderr);
-        async.each(self.depends[module], function(dep, done) {
-          fs.unlink(path.join(self.location[module], 'node_modules', dep));
-          var child = exec('npm link ' + module).stderr.pipe(process.stderr);
-        }, function(err) {
-          done(err);
-        });
+        var child = exec('npm link', function(err) {
+          if (err) return done(err);
+          async.each(self.depends[module], function(dep, done) {
+            fs.unlink(path.join(self.location[module], 'node_modules', dep), function(err) {
+              if (err) return done(err);
+              exec('npm link ' + module, function(err) {
+                done(err);
+              }).stderr.pipe(process.stderr);
+            });
+          }, function(err) {
+            done(err);
+          });
+        }).stderr.pipe(process.stderr);
       }, function(err) {
         if (callback) callback(err);
       });
     },
+
     unlink: function link(callback) {
       var self = this;
       async.each(self.modules, function(module, done) {
         async.each(self.depends[module], function(dep, done) {
-          fs.unlink(path.join(self.location[module], 'node_modules', dep));
+          fs.unlink(path.join(self.location[module], 'node_modules', dep), done);
         }, function(err) {
           if (err) return done(err);
           process.chdir(path.dirname(self.location[module]));
-          var child = exec('npm install').stderr.pipe(process.stderr);
-          done();
+          var child = exec('npm install', done).stderr.pipe(process.stderr);
         });
       }, function(err) {
         if (callback) callback(err);
       });
     },
+
     test: function test() {
       var self = this;
     }
